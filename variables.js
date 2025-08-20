c4672ce92253f0fc46ad0514c65551328a81237d
DenzelGitonga007 <gitongadenzel@gmail.com>
2025-08-20 20:37:22 +0300

    variables

---
+++ variables.js
@@ -0,0 +1,16 @@
+// Declaring a variable
+let name = "Peter Kamau"; // using let allows for reassignment later
+var age = 30; // old way of declaring variables, can be reassigned
+const country = "Kenya"; // constant variable, cannot be reassigned
+
+let city;
+city = "Nairobi"; // city is declared and then assigned a value
+
+const PI = 3.14159; // constant value for PI, cannot be changed
+// PI = 3.14; // This will throw an error because PI is a constant
+
+
+// print(name); // Output: Peter Kamau
+// print(age); // Output: 30   
+
+console.log(country); // Output: Kenya
